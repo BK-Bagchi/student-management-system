455de58111b6596ddd6aa4b28cec4036e4bc0d59
BK-Bagchi <bkbagchi.dipto@gmail.com>
2022-10-03 12:53:04 +0600

    layout created

--- students/views.py
+++ students/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
 
 def index(request):
-    return render(request, 'students/index.html')
+    students = Student.objects.all()
+    return render(request, 'students/index.html', context={'students': students})
