0dc598e0363d53ce8b3479816766f74e65449fe1
BK-Bagchi <bkbagchi.dipto@gmail.com>
2022-10-03 20:33:53 +0600

    record showing from db and modal added

--- students/views.py
+++ students/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
     students = Student.objects.all()
     return render(request, 'students/index.html', context={'students': students})
+
+
+def view_student(request, id):
+    students = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
