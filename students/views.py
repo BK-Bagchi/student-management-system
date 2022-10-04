1c475a1396fa6bb41b895c280a937b8d6dbe2348
BK-Bagchi <bkbagchi.dipto@gmail.com>
2022-10-04 17:20:57 +0600

    update and delete feature added and tested well

--- students/views.py
+++ students/views.py
@@ -35,9 +35,27 @@ def add(request):
                 gpa=new_gpa
             )
             new_student.save()
-
             return render(request, 'students/add.html', {'form': StudentForm(), 'success': True})
     else:
         form = StudentForm()
-
     return render(request, 'students/add.html', {'form': StudentForm()})
+
+
+def edit(request, id):
+    if request.method == 'POST':
+        student = Student.objects.get(pk=id)
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return render(request, 'students/edit.html', {'form': form, 'success': True})
+    else:
+        student = Student.objects.get(pk=id)
+        form = StudentForm(instance=student)
+    return render(request, 'students/edit.html', {'form': form})
+
+
+def delete(request, id):
+    if request.method == 'POST':
+        student = Student.objects.get(pk=id)
+        student.delete()
+    return HttpResponseRedirect(reverse('index'))
